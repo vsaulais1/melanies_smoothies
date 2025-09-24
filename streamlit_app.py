efc6c6b63735e3f6b505a76a6bd31aeb5007f6be
vsaulais1 <79105612+vsaulais1@users.noreply.github.com>
2025-09-24 10:34:35 +0100

    Update streamlit_app.py
    indentation fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,37 +6,36 @@ from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(f":cup_with_straw: Customise your Smoothie! :cup_with_straw:")
 st.write(
-"""Choose the fruits you want in your custom Smoothie!
- """
+    """Choose the fruits you want in your custom Smoothie!
+    """
 )
 
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be", name_on_order)
 
-
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-##st.dataframe(data=my_dataframe, use_container_width=True)
+## st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe, max_selections=5)
 
 if ingredients_list:
-ingredients_string = ''
+    ingredients_string = ''
 
-for fruit_chosen in ingredients_list:
-ingredients_string += fruit_chosen + ' '
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
 
-#st.write(ingredients_string)
+    # st.write(ingredients_string)
 
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-           values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+               values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
 
-time_to_insert = st.button('Submit Order')
+    time_to_insert = st.button('Submit Order')
 
-if time_to_insert:
-session.sql(my_insert_stmt).collect()
-st.success('Your Smoothie is ordered '+ name_on_order +'!', icon="✅")
-cnx= st.connexion("snowflake")
-session = cnx.session()
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered ' + name_on_order + '!', icon="✅")
+        cnx = st.connexion("snowflake")
+        session = cnx.session()
